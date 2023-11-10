2b13b10c1829019178cbe33a07d20314476006cd
willdjm <will_djm@hotmail.com>
2023-11-10 01:47:45 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'red': {
+          700: '#891127',
+          800: '#B20D1B',
+        },
+        'fuchsia': {
+          500: '#C400FF',
+        },
+        'blue': {
+          500: '#0062FF',
+        },
+        'sky': {
+          500: '#2177AD',
+        },
+        'red': {
+          500: '#FF0000',
+        },
+        'gray': {
+          300: '#F4F4F4',
+          500: '#8E8E8E',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
